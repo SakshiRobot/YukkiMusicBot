bf2ac49a95761e4e8b6b737ec3e22e5017946a3c
Sakshi <115390747+deepaiimss@users.noreply.github.com>
2022-10-29 02:35:49 +0530

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -57,9 +57,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TeamYM")
-                await self.one.join_chat("TheYukki")
-                await self.one.join_chat("YukkiSupport")
+                await self.one.join_chat("Aliza_update")
+                await self.one.join_chat("AlizaGbanLogs")
+                await self.one.join_chat("Aliza_support")
             except:
                 pass
             assistants.append(1)
@@ -88,9 +88,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("TeamYM")
-                await self.two.join_chat("TheYukki")
-                await self.two.join_chat("YukkiSupport")
+                await self.two.join_chat("Aliza_update")
+                await self.two.join_chat("AlizaGbanLogs")
+                await self.two.join_chat("Aliza_support")
             except:
                 pass
             assistants.append(2)
